51cde50093d354fe0ab93a04688971405d870588
jinny <jinny@jinny-laptop>
2011-02-10 08:32:04 +0900

    init rubyhub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyhub2::Application.config.secret_token = '7e3b0e5dde2ded488088300d786a80c742e7e3dec003e985480e1ce4a1219cd3494907eab120422da44612ac44d2d10c23ce3648ff25f24656287553ab719b16'
