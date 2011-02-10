916a8a7979ca3651dd0b9d2029f0c0b32cfd2210
jinny <jinny@jinny-laptop>
2011-02-11 08:37:34 +0900

    devise authentication  users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rubyhub2::Application.routes.draw do
+  #  devise_for :users, :path => "usuarios", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :registration => 'register'}
+  resources :users
+
+  get "home/index"
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
